239e3f0f6b781970b116006721aebe6de2540a7d
badri0909 <singuveerabhadrarao@gmail.com>
2022-02-22 17:06:56 +0530

    create Attendence Method

---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            reverse reverse = new reverse();
+            reverse.FindThePerfectNumber();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Employee/Employee/reverse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class reverse
+    {
+        public static void FindThePerfectNumber()
+        {
+            int num, sum, mn, mx;
+            Console.WriteLine("Finding perfect numbers in given series");
+            Console.WriteLine("Input the starting range");
+            mn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the ending range");
+            mx = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("perfect numbers are:");
+
+            for (num = mn; num <= mx; num++)
+            {
+                int i = 1;
+                sum = 0;
+                while (i < num)
+                {
+                    if (num % i == 0)
+                        sum += 1;
+                    i++;
+                }
+                if (sum == num)
+                    Console.Write(num + " ");
+            }
+        }
+    }
+
+}
+
